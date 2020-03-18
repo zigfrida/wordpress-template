e9032402a1766976be978774086eb19d29dd7cc0
Amanda Golubics <amanda.golubics@gmail.com>
2020-03-17 17:03:37 -0700

    Changes to category pg. Info animation working.

--- woocommerce-theme/src/category.js
+++ woocommerce-theme/src/category.js
@@ -2,10 +2,17 @@ let currentIndex = 0;
 let cat_array = [];
 let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-startLoad();
+const bottomNav = document.getElementById("bottom-navigation-contents");
+const bottomNavContents = jQuery("#bottom-navigation-contents");
+let categotyText = document.getElementById("category-text");
 
-function startLoad(){
+startLoad();
+firstSlideIn(0);
+
+function startLoad() {
     let cat_container = document.getElementsByClassName("cat-info");
+    let categories = document.getElementsByClassName("cat-nav-link");
+    categories[0].className += " active";
     for (let i = 0; i < cat_container.length; i++) {
         const div = cat_container[i];
 
@@ -15,28 +22,146 @@ function startLoad(){
             "name": div.children[1].value,
             "desc": div.children[2].value,
             "image": div.children[3].value,
+            "link": div.children[4].value,
         }
         cat_array.push(category);
     }
-    //console.log(cat_array);
 }
 
-function changeInfo(element){
-    let position = element.id;
+/***BOTTOM NAVIGATION FUNCTION */
+
+const limit = function () {
+    let numCategories = document.getElementsByClassName("cat-nav-link").length;
+    return ((bottomNav.scrollWidth - bottomNav.clientWidth) * -1);
+};
+
+jQuery("#right-arrow").click(function () { //Commented section show previous version
+    let move = widthToMove();
+    //let currentPosition = parseInt(bottomNavContents.css("left"));
+    let currentPosition = parseInt(bottomNavContents.scrollLeft());
+    //let currentPosition = parseFloat(bottomNavContents.getBoundingClientRect().left);
+    if (currentPosition >= limit()) {
+        bottomNavContents.animate({
+            scrollLeft: "+=" + move
+        }, 400);
+        // bottomNavContents.stop(false, true).animate({ left: "-=" + move }, { duration: 400 })
+    }
+});
+
+jQuery("#left-arrow").click(function () {
+    let move = widthToMove();
+    let currentPosition = parseInt(bottomNavContents.scrollLeft());
+    if (currentPosition > 0) { // < before
+        bottomNavContents.animate({
+            scrollLeft: "-=" + move
+        }, 400);
+    }
+});
+
+function widthToMove() {
+    let allCategories = document.getElementsByClassName("cat-nav-link");
+
+    //Getting properties of a single category showing on screen
+    let element = allCategories[0];
+    let width = element.getBoundingClientRect().width;
+    let marginStyle = window.getComputedStyle(element);
+    let marginRight = parseFloat(marginStyle.getPropertyValue('margin-right').split("px")[0]);
+    let marginLeft = parseFloat(marginStyle.getPropertyValue('margin-left').split("px")[0]);
+    let fullElementWidth = width + marginLeft + marginRight;
+
+    // Bottom Nav width hidden due to overflow
+    let maxBottomNavWidth = (bottomNav.scrollWidth - bottomNav.clientWidth);
+    //Num categories currently showing
+    let numCategoriesShowing = bottomNav.clientWidth / fullElementWidth;
+    //Num categories hidden due to overflow
+    let numCategoriesHidden = allCategories.length - numCategoriesShowing;
+    //Value for the scroll bar to move is the width hidden due to overflow divided by how many categories are hidden 
+    let valueToMove = maxBottomNavWidth / numCategoriesHidden;
+    return valueToMove;
+}
+
+/**MIDDLE CONTENT - TEXT ANIMATION AND CLICK EVENTS */
+
+jQuery(".cat-nav-link").on('click', function (event) {
+    let indexClicked = jQuery('.cat-nav-link').index(this);
+    let currentActive = document.getElementsByClassName("active")[0];
+    let currentActiveIndex = jQuery('.cat-nav-link').index(currentActive);
+    if (indexClicked == currentActiveIndex) {
+        return;
+    } else if (indexClicked > currentActiveIndex) {
+        goForward(indexClicked);
+    } else {
+        goBack(indexClicked);
+    }
+    currentActive.className = currentActive.className.replace(" active", "");
+    this.className += " active";
+});
+
+function replaceInfo(id_clicked) {
+    let position = id_clicked;
     let category = cat_array[position];
     document.getElementById("category-name").textContent = category.name;
     document.getElementById("category-desc").textContent = category.desc;
-    //document.getElementById("category-link").href = category.;
+    document.getElementById("category-img").src = category.image;
+    document.getElementById("category-img").alt = category.name;
+    document.getElementById("category-link").href = category.link;
 }
 
-function goForward() {
+function rmvPreviousPrt() {
+    categotyText.style.removeProperty("transition");
+    categotyText.classList.remove("runSlideNextAnimation");
+    categotyText.classList.remove("runSlideNextHideAnimation");
+    categotyText.classList.remove("runSlideNextReverseAnimation");
+    categotyText.classList.remove("runSlideNextHideReverseAnimation");
+    void categotyText.offsetWidth;
+}
+
+function firstSlideIn(id) {
+    if (id !== undefined) {
+        replaceInfo(id);
+    }
+    categotyText.classList.add("runSlideNextHideReverseAnimation");
+    categotyText.style.transition = "width 0.5s";
+    categotyText.style.width = "50vw";
+}
+
+function hide_current_for_next() {
+    rmvPreviousPrt();
+    categotyText.classList.add("runSlideNextHideAnimation");
+    categotyText.style.transition = "width 0.6s";
+    categotyText.style.width = "0";
+}
+
+function slide_in_next() {
+    rmvPreviousPrt();
+    categotyText.classList.add("runSlideNextAnimation");
+    categotyText.style.transition = "width 0.5s";
+    categotyText.style.width = "50vw";
+}
+
+function hide_current_for_previous() {
+    rmvPreviousPrt();
+    categotyText.classList.add("runSlideNextReverseAnimation");
+    categotyText.style.transition = "width 0.5s";
+    categotyText.style.width = "0";
+}
+
+function goForward(id) {
+    hide_current_for_next();
     overlayHideLeft();
-    setTimeout(overlayShowLeft, 1000);
+    setTimeout(function () {
+        overlayShowLeft(id);
+    }, 1000);
+    setTimeout(slide_in_next, 1300);
 }
 
-function goBack() {
+function goBack(id) {
+    hide_current_for_previous();
     overlayHideRight();
-    setTimeout(overlayShowRight, 1000);
+    setTimeout(function () {
+        overlayShowRight(id);
+    }, 1000);
+    setTimeout(firstSlideIn, 1300);
 }
 
 function overlayHideLeft() {
@@ -47,7 +172,8 @@ function overlayHideLeft() {
     overlay.style.width = "100%";
 }
 
-function overlayShowLeft() {
+function overlayShowLeft(id) {
+    replaceInfo(id);
     let overlay = document.getElementById("cat-overlay");
     overlay.style.removeProperty('right');
     overlay.style.left = "0";
@@ -59,55 +185,15 @@ function overlayHideRight() {
     let overlay = document.getElementById("cat-overlay");
     overlay.style.removeProperty('right');
     overlay.style.left = "0";
-    overlay.style.transition = "width 1.2s";
+    overlay.style.transition = "width 1s";
     overlay.style.width = "100%";
 }
 
-function overlayShowRight() {
+function overlayShowRight(id) {
+    replaceInfo(id);
     let overlay = document.getElementById("cat-overlay");
     overlay.style.removeProperty('left');
     overlay.style.right = "0";
-    overlay.style.transition = "width 1.2s";
+    overlay.style.transition = "width 1s";
     overlay.style.width = "0";
 }
-
-
-var bottomNavContents = jQuery("#bottom-navigation-contents");
-
-let limit = -750;
-
-jQuery("#right-arrow").click(function(){
-    let move = widthToMove();
-    let currentPosition = parseInt(bottomNavContents.css("left"));
-    console.log(currentPosition);
-    if (currentPosition >= limit) {
-        bottomNavContents.stop(false, true).animate({ left: "-=" + move }, { duration: 400 })
-    }
-});
-
-jQuery("#left-arrow").click(function () {
-    let move = widthToMove();
-    let currentPosition = parseInt(bottomNavContents.css("left"));
-    console.log(currentPosition);
-    if (currentPosition < 0) {
-        bottomNavContents.stop(false, true).animate({ left: "+=" + move }, { duration: 400 })
-    }
-});
-
-function widthToMove(){
-    let element = document.getElementsByClassName("cat-nav-link")[0];
-    let width = element.clientWidth;
-    let marginStyle = window.getComputedStyle(element);
-    let marginRight = parseInt(marginStyle.getPropertyValue('margin-right').split("px")[0]);
-    let marginLeft = parseInt(marginStyle.getPropertyValue('margin-left').split("px")[0]);
-    console.log(width + marginRight + marginLeft);
-    return width + marginRight + marginLeft;
-}
-
-
-(function (){
-    // let numCategories = document.getElementsByClassName("cat-nav-link").length;
-    // let categoriesWidth = widthToMove();
-    // let nav = document.getElementById("bottom-navigation-contents");
-    // nav.style.width = (numCategories * categoriesWidth) + "px";
-})();
--- woocommerce-theme/template-category.php
+++ woocommerce-theme/template-category.php
@@ -9,6 +9,7 @@
     <div id="cat-overlay"></div>
 
     <div id="current-cat">
+        <img id="category-img">
         <?php
             $num_cat = 0;
             $taxonomy     = 'product_cat';
@@ -38,71 +39,66 @@
 
     </div>
 
-    <div id="category-info" class="d-flex flex-column">
-        <h1 class="p-2 text-center" id="category-name">Name</h1>
-        <h4 class="p-2 text-center" id="category-desc">Description @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@hhhhhhhhhhhhh@@@@@</h4>
-        <a id="category-link" class="p-2 text-center">View More</a>
+    <div id="category-text"> 
+        <div id="category-info" class="d-flex flex-column">
+            <h1 class="p-2 text-center" id="category-name">Name</h1>
+            <h4 class="p-2 text-center" id="category-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pretium metus sed dolor interdum, vel laoreet diam varius. Duis ac nunc nec erat auctor pretium ac consectetur velit. Morbi ut ultricies dolor. In lacinia, erat et sagittis blandit, orci diam accumsan quam, vitae varius arcu tellus et leo. Nunc lacinia elit at suscipit pretium. Fusce semper dolor a purus mollis, id tristique lectus vestibulum. Cras nec faucibus enim, eu faucibus metus. Vestibulum a fermentum felis. Aliquam in eros quis lectus sagittis lacinia eu sit amet est. Vivamus id dapibus tortor. In dictum ut sapien sed tincidunt.</h4>
+            <a id="category-link" class="p-2 text-center">View More</a>
+        </div>
     </div>
 
     <div id="bottom-navigation" class="text-center">
 
         <div id="left-arrow" class="arrow" ><i class="fa fa-chevron-left"></i></div>
-        
-        <!-- <div id="bottom-outer-nav-contents"> -->
-            <div id="bottom-navigation-contents">
-                <div class="cat-nav-link"><span class="cat-id">01</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">02</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">03</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">04</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">05</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">06</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">07</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">08</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">09</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">10</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">11</span><span class="cat-name">This Name Photo</span></div>
-                <div class="cat-nav-link"><span class="cat-id">12</span><span class="cat-name">This Name Photo</span></div>
-                <?php
-                    $num_cat = 0;
-                    $taxonomy     = 'product_cat';
-                    $orderby      = 'name';  
-                    $show_count   = 0;      // 1 for yes, 0 for no
-                    $pad_counts   = 0;      // 1 for yes, 0 for no
-                    $hierarchical = 1;      // 1 for yes, 0 for no  
-                    $title        = '';  
-                    $empty        = 0;
 
-                    $args = array(
-                            'taxonomy'     => $taxonomy,
-                            'orderby'      => $orderby,
-                            'show_count'   => $show_count,
-                            'pad_counts'   => $pad_counts,
-                            'hierarchical' => $hierarchical,
-                            'title_li'     => $title,
-                            'hide_empty'   => $empty
-                    );
-                    $all_categories = get_categories( $args );
-                    $num_cat = count($all_categories);
+        <div id="bottom-navigation-contents">
+            <!-- <div class="cat-nav-link active"><span class="cat-id">01</span><span class="cat-name">This Name Photo</span></div> -->
 
-                    for ($i = 0; $i < $num_cat; $i++) {
-                        $cat = $all_categories[$i];
-                        $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
-                        $image = wp_get_attachment_url( $thumbnail_id );
-                        $cat_link = esc_url(get_category_link($cat->term_id));
-                        $current_id = "cat-" . $i;
+            <?php
+                $num_cat = 0;
+                $taxonomy     = 'product_cat';
+                $orderby      = 'name';  
+                $show_count   = 0;      // 1 for yes, 0 for no
+                $pad_counts   = 0;      // 1 for yes, 0 for no
+                $hierarchical = 1;      // 1 for yes, 0 for no  
+                $title        = '';  
+                $empty        = 0;
 
-                        // echo "<div id='$i' class='cat-nav-link' onclick='changeInfo(this)'><span>$id</span><span>$cat->name</span></div>";
-                        
-                        echo "<div id='$cat->term_id' class='cat-info'>";
-                        echo "<input type='hidden' value='$i'>";
-                        echo "<input type='hidden' value='$cat->name'>";
-                        echo "<input type='hidden' value='$cat->description'>";
-                        echo "<input type='hidden' value='$image'>";
-                        echo '</div>';
-                    }
-                ?>
-            </div>
-        <!-- </div> -->
+                $args = array(
+                        'taxonomy'     => $taxonomy,
+                        'orderby'      => $orderby,
+                        'show_count'   => $show_count,
+                        'pad_counts'   => $pad_counts,
+                        'hierarchical' => $hierarchical,
+                        'title_li'     => $title,
+                        'hide_empty'   => $empty
+                );
+                $all_categories = get_categories( $args );
+                $num_cat = count($all_categories);
+
+                for ($i = 0; $i < $num_cat; $i++) {
+                    $cat = $all_categories[$i];
+                    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+                    $image = wp_get_attachment_url( $thumbnail_id );
+                    $cat_link = esc_url(get_category_link($cat->term_id));
+
+                    $cat_id = $i + 1;
+
+                    echo "<div class='cat-nav-link'>";
+                    echo "<span class='cat-id'>$cat_id</span>";
+                    echo "<span class='cat-name'>$cat->name</span>";
+                    echo "</div>";
+
+                    echo "<div id='$cat->term_id' class='cat-info'>";
+                    echo "<input type='hidden' value='$i'>";
+                    echo "<input type='hidden' value='$cat->name'>";
+                    echo "<input type='hidden' value='$cat->description'>";
+                    echo "<input type='hidden' value='$image'>";
+                    echo "<input type='hidden' value=' $cat_link'>";
+                    echo '</div>';
+                }
+            ?>
+        </div>
 
         <div id="right-arrow" class="arrow"><i class="fa fa-chevron-right"></i></div>
 

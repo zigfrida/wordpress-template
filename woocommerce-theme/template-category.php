c852841a6504c7af286705cbf97639c2b0071e4e
Amanda Golubics <amanda.golubics@gmail.com>
2020-03-17 17:42:19 -0700

    Changes to templates with new WooCommerce update.

--- woocommerce-theme/template-category.php
+++ woocommerce-theme/template-category.php
@@ -10,40 +10,13 @@
 
     <div id="current-cat">
         <img id="category-img">
-        <?php
-            $num_cat = 0;
-            $taxonomy     = 'product_cat';
-            $orderby      = 'name';  
-            $show_count   = 0;      // 1 for yes, 0 for no
-            $pad_counts   = 0;      // 1 for yes, 0 for no
-            $hierarchical = 1;      // 1 for yes, 0 for no  
-            $title        = '';  
-            $empty        = 0;
-
-            $args = array(
-                    'taxonomy'     => $taxonomy,
-                    'orderby'      => $orderby,
-                    'show_count'   => $show_count,
-                    'pad_counts'   => $pad_counts,
-                    'hierarchical' => $hierarchical,
-                    'title_li'     => $title,
-                    'hide_empty'   => $empty
-            );
-            $all_categories = get_categories( $args );
-            $cat = $all_categories[0];
-            $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
-            $image = wp_get_attachment_url( $thumbnail_id );
-            $cat_link = esc_url(get_category_link($cat->term_id));
-            echo "<img src='$image' alt='$cat->name'>";
-        ?>
-
     </div>
 
     <div id="category-text"> 
         <div id="category-info" class="d-flex flex-column">
-            <h1 class="p-2 text-center" id="category-name">Name</h1>
-            <h4 class="p-2 text-center" id="category-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pretium metus sed dolor interdum, vel laoreet diam varius. Duis ac nunc nec erat auctor pretium ac consectetur velit. Morbi ut ultricies dolor. In lacinia, erat et sagittis blandit, orci diam accumsan quam, vitae varius arcu tellus et leo. Nunc lacinia elit at suscipit pretium. Fusce semper dolor a purus mollis, id tristique lectus vestibulum. Cras nec faucibus enim, eu faucibus metus. Vestibulum a fermentum felis. Aliquam in eros quis lectus sagittis lacinia eu sit amet est. Vivamus id dapibus tortor. In dictum ut sapien sed tincidunt.</h4>
-            <a id="category-link" class="p-2 text-center">View More</a>
+            <h1 class="p-2 text-center" id="category-name"></h1>
+            <h4 class="p-2 text-center" id="category-desc"></h4>
+            <a id="category-link" class="p-2 text-center"></a>
         </div>
     </div>
 
@@ -52,15 +25,13 @@
         <div id="left-arrow" class="arrow" ><i class="fa fa-chevron-left"></i></div>
 
         <div id="bottom-navigation-contents">
-            <!-- <div class="cat-nav-link active"><span class="cat-id">01</span><span class="cat-name">This Name Photo</span></div> -->
-
             <?php
                 $num_cat = 0;
                 $taxonomy     = 'product_cat';
                 $orderby      = 'name';  
-                $show_count   = 0;      // 1 for yes, 0 for no
-                $pad_counts   = 0;      // 1 for yes, 0 for no
-                $hierarchical = 1;      // 1 for yes, 0 for no  
+                $show_count   = 0;      
+                $pad_counts   = 0;      
+                $hierarchical = 1;        
                 $title        = '';  
                 $empty        = 0;
 

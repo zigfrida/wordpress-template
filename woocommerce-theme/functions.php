45315c8a522fcc0a29fef9ebdfa0d52a56a3a683
zigfrida <amanda.golubics@gmail.com>
2020-07-29 09:27:21 -0700

    Styling for single product page.

--- woocommerce-theme/functions.php
+++ woocommerce-theme/functions.php
@@ -84,6 +84,9 @@ register_sidebar(
  //Declaring WooCommerce Support
 function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
+    // add_theme_support( 'wc-product-gallery-zoom' ); 
+    // add_theme_support( 'wc-product-gallery-lightbox' ); 
+    // add_theme_support( 'wc-product-gallery-slider' );
 }
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );

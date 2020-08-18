8006a1243474e2adf8145d89525365baf1d0ff2f
zigfrida <amanda.golubics@gmail.com>
2020-08-17 21:26:30 -0700

    Finish styling single product page. Plus plugin updates.

--- woocommerce-theme/functions.php
+++ woocommerce-theme/functions.php
@@ -15,10 +15,10 @@ function load_stylesheets(){
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
 function load_javascript(){
-    wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', array('jquery'), 1, true);
     wp_enqueue_script('custom');
 
-    wp_register_script('header_animation', get_template_directory_uri() . '/src/js/header_animation.js', 'jquery', 1, true);
+    wp_register_script('header_animation', get_template_directory_uri() . '/src/js/header_animation.js', array('jquery'), 1, true);
     wp_enqueue_script('header_animation');
 
     if(is_page()){
@@ -31,6 +31,8 @@ function load_javascript(){
         } else if(is_front_page()){
             wp_register_script('front-page', get_template_directory_uri() . '/src/js/front_page.js', 'jquery', 1, true);
             wp_enqueue_script('front-page');
+        }else if(is_product()){
+            // Single Product page?
         }
     }
 
@@ -84,17 +86,34 @@ register_sidebar(
  //Declaring WooCommerce Support
 function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
-    // add_theme_support( 'wc-product-gallery-zoom' ); 
-    // add_theme_support( 'wc-product-gallery-lightbox' ); 
-    // add_theme_support( 'wc-product-gallery-slider' );
+    //add_theme_support( 'wc-product-gallery-zoom' );   //Zoom when hoover
+    add_theme_support( 'wc-product-gallery-lightbox' ); 
+    //add_theme_support( 'wc-product-gallery-slider' );
+
+    // Theme no longer use the WooCommerce stylesheet, giving a blank canvas.
+    add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+    add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+    add_filter( 'gettext', 'custom_related_products_text', 20, 3 );
+    add_filter( 'woocommerce_is_sold_individually','custom_remove_all_quantity_fields', 10, 2 );
+    add_filter('genesis_post_meta', 'crunchify_post_meta');
+    add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' ); 
+    // Trim zeros in price decimals
+    add_filter( 'woocommerce_price_trim_zeros', '__return_true' );
+    //Change number of related products output
+    add_filter( 'woocommerce_output_related_products_args', 'max_related_products_args', 20 );
+
 }
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
-// Theme no longer use the WooCommerce stylesheet, giving a blank canvas.
-add_filter( 'woocommerce_enqueue_styles', '__return_false' );
-
-add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+function custom_related_products_text( $translated_text, $text, $domain ) {
+    switch ( $translated_text ) {
+      case 'Related products' :
+        $translated_text = __( 'Similar', 'woocommerce' );
+        break;
+    }
+    return $translated_text;
+  }
 
 function woo_remove_product_tabs( $tabs ) {
     unset( $tabs['description'] );          // Remove the description tab
@@ -107,17 +126,38 @@ function woo_remove_product_tabs( $tabs ) {
 function custom_remove_all_quantity_fields( $return, $product ) {
     return true;
 }
-add_filter( 'woocommerce_is_sold_individually','custom_remove_all_quantity_fields', 10, 2 );
-
-add_filter('genesis_post_meta', 'crunchify_post_meta');
 
 // To change add to cart text on product archives(Collection) page
 function woocommerce_custom_product_add_to_cart_text() {
     return __( 'Download', 'woocommerce' );
-}
-add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
+} 
 
 /**
- * Trim zeros in price decimals
- **/
-add_filter( 'woocommerce_price_trim_zeros', '__return_true' );
+ * Change number of related products output
+ */ 
+function max_related_products_args( $args ) {
+    $args['posts_per_page'] = 3; // 4 related products
+    $args['columns'] = 2; // arranged in 2 columns
+    return $args;
+}
+
+
+
+add_filter('wp_handle_upload_prefilter', 'limit_wp_handle_upload_prefilter');
+function yoursite_wp_handle_upload_prefilter($file) {
+  // This bit is for the flash uploader
+  if ($file['type']=='application/octet-stream' && isset($file['tmp_name'])) {
+    $file_size = getimagesize($file['tmp_name']);
+    if (isset($file_size['error']) && $file_size['error']!=0) {
+      $file['error'] = "Unexpected Error: {$file_size['error']}";
+      return $file;
+    } else {
+      $file['type'] = $file_size['mime'];
+    }
+  }
+  if ($post_id = (isset($_REQUEST['post_id']) ? $_REQUEST['post_id'] : false)) {
+    if (count(get_posts("post_type=attachment&post_parent={$post_id}"))>3)
+      $file['error'] = "Sorry, you cannot upload more than four (4) image.";
+  }
+  return $file;
+}
--- woocommerce-theme/woocommerce/woocommerce.js
+++ woocommerce-theme/woocommerce/woocommerce.js
@@ -93,44 +93,11 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// jQuery(window).on('load', function() {
-//   var form = document.getElementsByClassName("cart")[0];
-//   console.log(form);
-//   var outter_div = document.createElement("div");
-//   outter_div.setAttribute("class", "svg-wrapper");
-//   var svg = document.createElement("svg");
-//   // svg.setAttribute("class","style-svg");
-//   //svg.setAttribute("preserveAspectRatio", "none");
-//   svg.style.height = "60px";
-//   svg.style.width = "320px";
-//   // svg.setAttribute("height", "60");
-//   // svg.setAttribute("width", "320");
-//   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-//   var rect = document.createElement("rect");
-//   rect.setAttribute("class", "shape");
-//   rect.style.height = "60px";
-//   rect.style.width = "320px";
-//   // rect.setAttribute("height", "60");
-//   // rect.setAttribute("width", "320");
-//   // rect.textContent = " T   ";
-//   var btn = document.getElementsByClassName("single_add_to_cart_button")[0];
-//   // btn.className = btn.className + " text";
-//   var text_space = document.createElement("div");
-//   text_space.setAttribute("class", "text");
-//   text_space.textContent = "This is a test";
-//   svg.appendChild(rect);
-//   //svg.appendChild(text_space);
-//   outter_div.appendChild(svg);
-//   outter_div.appendChild(text_space);
-// //   //outter_div.appendChild(btn);
-//   //form.appendChild(outter_div);
-//   // //svg.appendChild(btn);
-//   // //rect.appendChild(btn);
-//   var x = document.getElementById("container-product-info");
-//   x.appendChild(outter_div);
-//   // var y = document.getElementsByClassName("svg")[0].getSVGDocumnet();
-//   // console.log(y);
-// });
+var product = document.getElementsByClassName('single-product')[0];
+var image = document.getElementsByClassName('wp-post-image')[0];
+var image_src = image.src;
+console.log(image_src);
+product.style.backgroundImage = 'url(' + image_src + ')';
 
 /***/ }),
 
